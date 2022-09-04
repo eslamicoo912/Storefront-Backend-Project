5bd24f7e9b78961f21859490657cb1fb590ad7d5
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-05 00:19:50 +0200

    Add the authentication to the users route

--- src/routes/api/users.route.ts
+++ src/routes/api/users.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import * as controllers from '../../controllers/users.controller'
+import authenticationMiddleware from '../../middleware/authentication.middleware'
 
 const routes = Router()
 
 routes.post('/', controllers.createUser)
-routes.get('/', controllers.getMany)
+routes.get('/', authenticationMiddleware, controllers.getMany)
 routes.get('/:id', controllers.getOne)
 routes.patch('/:id', controllers.updateOne)
 routes.delete('/:id', controllers.deleteOne)

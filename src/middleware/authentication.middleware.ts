62d8675e61d65a9232bb054f23f63c244a6e0a12
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-06 18:52:10 +0200

    Add tokens to user routes

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -2,14 +2,9 @@ import config from '../config'
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
-const handleAuthenticationError = (next: NextFunction) => {
-  const error: Error = new Error('Login error')
-  next(error)
-}
-
-const validateToken = async (req: Request, res: Response, next: NextFunction) => {
+export const validateToken = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const authHeader = req.get('Authorization')
+    const authHeader = req.headers.authorization
     if (authHeader) {
       const bearer = authHeader.split(' ')[0].toLowerCase()
       const token = authHeader.split(' ')[1]
@@ -17,18 +12,10 @@ const validateToken = async (req: Request, res: Response, next: NextFunction) =>
         const decode = jwt.verify(token, config.token as unknown as string)
         if (decode) {
           next()
-        } else {
-          handleAuthenticationError(next)
         }
-      } else {
-        handleAuthenticationError(next)
       }
-    } else {
-      handleAuthenticationError(next)
     }
   } catch (error) {
     next(error)
   }
 }
-
-export default { validateToken }
--- src/routes/api/users.route.ts
+++ src/routes/api/users.route.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express'
 import * as controllers from '../../controllers/users.controller'
-import authenticationMiddleware from '../../middleware/authentication.middleware'
+import { validateToken } from '../../middleware/authentication.middleware'
 
 const routes = Router()
 
-routes.post('/', controllers.createUser)
-routes.get('/', controllers.getMany)
-routes.get('/:id', controllers.getOne)
+routes.post('/', validateToken, controllers.createUser)
+routes.get('/', validateToken, controllers.getMany)
+routes.get('/:id', validateToken, controllers.getOne)
 routes.patch('/:id', controllers.updateOne)
-routes.delete('/:id', controllers.deleteOne)
+routes.delete('/:id', validateToken, controllers.deleteOne)
 routes.post('/authenticate', controllers.authenticate)
 
 export default routes

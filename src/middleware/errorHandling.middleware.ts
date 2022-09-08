a972142d14aa27487093ff00d67cd1afddb979b9
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-08 10:47:38 +0200

    Delete error middleware file

--- src/middleware/errorHandling.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express'
-import Error from '../interface/Error'
-const errorMiddleware = (error: Error, req: Request, res: Response) => {
-  const status: number = error.status || 500
-  const message: string = error.message || 'error message'
-  res.status(status).json({ status, message })
-}
-
-export default errorMiddleware
